7080130d0a1a0401e85f76a476f6b80b34c59739
amdpt323 <samandeep21@iitk.ac.in>
2022-12-31 23:53:37 +0530

    some ui changes in about-me

--- components/layout/amSidebar.js
+++ components/layout/amSidebar.js
@@ -1,8 +1,22 @@
+import { BsTerminalFill } from 'react-icons/bs'
+import{AiFillBulb, AiFillProject} from 'react-icons/ai'
+import { MdKeyboardArrowDown } from 'react-icons/md'
+
 export default function ASidebar({ children }) {
   return (
-    <>
-      <p>About Me sidebar</p>
+    <div className='main'>
+      <div className='asb'>
+        <BsTerminalFill className='asb-icons' />
+        <AiFillBulb className='asb-icons bulb' />
+        <AiFillProject className='asb-icons' />
+      </div>
+      <div className='amsb'>
+        <div className='flexhsb'>
+          <MdKeyboardArrowDown className='amsb-icons'/>
+          <p>personal-info</p>
+        </div>
+      </div>
       {children}
-    </>
+    </div>
   )
 }
